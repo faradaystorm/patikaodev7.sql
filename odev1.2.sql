383537b13481e98b3244bbc6f51a29ceb6f1329b
Esma Şen <96180440+faradaystorm@users.noreply.github.com>
2022-07-22 17:04:49 +0300

    Create odev1.2.sql

---
+++ odev1.2.sql
@@ -0,0 +1,4 @@
+--film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+SELECT replacement_cost FROM film
+GROUP BY replacement_cost
+HAVING COUNT (*) > 50;
