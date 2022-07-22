2f8eb7c17a1a06272005533019711c1006a7d24b
Esma Şen <96180440+faradaystorm@users.noreply.github.com>
2022-07-22 17:16:42 +0300

    Create odev1.4.sql

---
+++ odev1.4.sql
@@ -0,0 +1,5 @@
+--city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+SELECT country_id, COUNT (*) FROM city
+GROUP BY country_id
+ORDER BY COUNT (*) DESC
+LIMIT 1;
